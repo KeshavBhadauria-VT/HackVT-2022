f726f0c419b20a8ce92c9f1f5628981adc082361
Keshav Bhadauria <keshav@vt.edu>
2022-11-12 17:59:30 -0500

    apply, accept, accepted

--- commands/accepted.js
+++ commands/accepted.js
@@ -7,7 +7,9 @@ module.exports = {
         .setName('accepted')
         .setDescription('list the companies you were accepted by'),
 	async execute(client, interaction, args = []) {
+
         let user_that_called_command = await User.findById(interaction.user.id);
+
         let str = `${user_that_called_command.name}\n`;
         for (let i = 0; i < user_that_called_command.applications.length; i++) {
             if (user_that_called_command.applications[i].status === "accepted") {
--- index.js
+++ index.js
@@ -60,7 +60,12 @@ client.on(Events.InteractionCreate, async (interaction) => {
     } else if (interaction.commandName === "remove_friend") {
       await interaction.deferReply();
       await command.execute(client, interaction, []);
-    } else {
+    } else if (interaction.commandName === "apply") {
+      await interaction.deferReply();
+      await command.execute(client, interaction, []);
+
+    } 
+    else {
       await command.execute(client, interaction, []);
     }
   } catch (error) {
