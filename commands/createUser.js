b09655d6e1ed828580e3469a2162cbbccd53af06
Keshav Bhadauria <keshav@vt.edu>
2022-11-12 14:35:53 -0500

    createUser now

---
+++ commands/createUser.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+const mongoose = require('mongoose');
+const User = require("../schemas/User");
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('create_user')
+		.setDescription('Creates user info')
+        .addUserOption(option => 
+            option.setName('user')
+            .setDescription('user you want to create')
+            .setRequired(true)),
+	async execute(client, interaction, args = []) {
+        let guild_member = await interaction.guild.members.cache.get(`${interaction.options.getUser('user').id}`);
+        
+        const user = await User.create({
+            _id: interaction.options.getUser('user').id, 
+            name: guild_member.user.username,
+            createdAt: guild_member.joinedAt,
+            applications: []
+
+        });
+
+        
+		await interaction.editReply(`${user}`);
+	},
+};
