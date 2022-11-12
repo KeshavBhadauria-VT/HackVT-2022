632acdaff123230f6b9a71d1b559b198e12ad48a
Silver Harris <silverharris@vt.edu>
2022-11-12 18:25:22 -0500

    get info

--- commands/getServerInfo.js
+++ commands/getServerInfo.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-
+const User = require('../schemas/User');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("serverinfo")
@@ -10,12 +10,32 @@ module.exports = {
    * {string[]}args -> extra args.
    */
   async execute(client, interaction, args = []) {
-    let users = ``;
-    client.guilds.cache.forEach((g) => {
-      g.members.cache.forEach((m) => {
-        users += `${m.user.username}\n \tjoined: ${m.joinedAt}\n \tapplied to X companies\n \tX acceptance\n \tX Ghosts\n \tX Rejections\n`;
+    let info = ``;
+    client.guilds.cache.forEach().then((g) => {
+      g.members.cache.forEach(async (m) => {
+        info += `${m.user.username}\n \tjoined: ${m.joinedAt}\n `;
+        
+        console.log(m.user.id);
+        let user =  await User.findById(m.user.id);
+        //let applications = m.user.applications;
+        let following_me = user.following_me;
+
+        info += "\tGhosted by" + `${await user.applies}` + "companies.\n";
+        info += "\tRejected from " + `${await user.rejects}` + " companies.\n";
+        info += "\tAccepted by " + `${await user.accepts}` + " companies. \n";
+        info += "\tCompanies: \n";
+
+        // for (const application of applications) {
+        //   info += "\t\t" + application.company.name + ":\t";
+        //   info += application.status + "\n";
+        // }
+        // info += "\tFollowing me: \n";
+        // for (const friend of following_me) {
+        //   info += "\t\t" + friend.name + "\n";
+        // }
+
       });
     });
-    await interaction.editReply(users);
+    await interaction.editReply(info);
   },
 };
