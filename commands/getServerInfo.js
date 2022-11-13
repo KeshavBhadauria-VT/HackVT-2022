d5b15eb4055c0c60f8b480359ad0059bd9ea8a7e
Silver Harris <silverharris@vt.edu>
2022-11-12 20:03:09 -0500

    server info

--- commands/getServerInfo.js
+++ commands/getServerInfo.js
@@ -1,41 +1,37 @@
-const { SlashCommandBuilder } = require("discord.js");
-const User = require('../schemas/User');
+const { SlashCommandBuilder, Collection } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const User = require("../schemas/User");
+
+
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("serverinfo")
     .setDescription("gets everyone in the server"),
-  /*
-   * {client object} client -> passing in client for queries
-   * {interaction} -> how you send different response signals
-   * {string[]}args -> extra args.
-   */
   async execute(client, interaction, args = []) {
-    let info = ``;
-    client.guilds.cache.forEach().then((g) => {
-      g.members.cache.forEach(async (m) => {
-        info += `${m.user.username}\n \tjoined: ${m.joinedAt}\n `;
-        
-        console.log(m.user.id);
-        let user =  await User.findById(m.user.id);
-        //let applications = m.user.applications;
-        let following_me = user.following_me;
+    let info = `Information:\n`;
+    for await (const doc of User.find()) {
 
-        info += "\tGhosted by" + `${await user.applies}` + "companies.\n";
-        info += "\tRejected from " + `${await user.rejects}` + " companies.\n";
-        info += "\tAccepted by " + `${await user.accepts}` + " companies. \n";
-        info += "\tCompanies: \n";
+      info += `${doc.name}\n \tjoined: ${doc.createdAt}\n `;
+      info += "\tGhosted by " + `${await doc.applies}` + " companies.\n";
+      info += "\tRejected from " + `${await doc.rejects}` + " companies.\n";
+      info += "\tAccepted by " + `${await doc.accepts}` + " companies. \n";
+      let applications = await doc.applications;
+      let following_me = await doc.following_me;
+      info += "\tCompanies: \n";
+      for (const application of applications) {
+        info += "\t\t" + application.company.name + ":\t";
+        info += application.status + "\n";
+      }
+      info += "\tFollowing me: \n";
+      for (const friend of following_me) {
+        info += "\t\t" + friend.name + "\n";
+      }
 
-        // for (const application of applications) {
-        //   info += "\t\t" + application.company.name + ":\t";
-        //   info += application.status + "\n";
-        // }
-        // info += "\tFollowing me: \n";
-        // for (const friend of following_me) {
-        //   info += "\t\t" + friend.name + "\n";
-        // }
+      // use `doc`
+    }
+    await interaction.editReply(`${info}`);
 
-      });
-    });
-    await interaction.editReply(info);
   },
 };
